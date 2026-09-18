e95e6a1b3cbc66e03919246a2904ec2aa81d1ca8
Padmapriya A <padmapriyaa@systechusa.com>
2026-09-18 16:26:57 +0000

    pivot_jinja

---
+++ models/examples/stage/jinja_pivot.sql
@@ -0,0 +1,18 @@
+with amt as (
+    select * from 
+    {{ref ('stg_order')}}
+    where status='Delivered'
+)
+,pivoted as (
+    select ORDER_ID,
+    {%set Categorys =['Electronics','Home','Sports','Grocery','Office']%}
+
+    {% for category in Categorys %}
+        sum(case when category ='{{category}}' then amount else 0 end) as {{category}}_amount
+        
+    {% endfor %}
+    from amt
+    group by ORDER_ID
+)
+select * from  pivoted
+
